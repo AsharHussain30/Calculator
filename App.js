4b7d54c2133e23c452dc557ab6c9eb22b8b0742f
AsharHussain30 <120649337+AsharHussain30@users.noreply.github.com>
2022-12-15 19:00:36 +0500

    App.js

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import {Calculator} from './android/app/src/assets/Calculator'
-import { Provider } from 'react-redux';
-import store from './store/store';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {Cart} from './android/app/src/assets/Cart'
-import {Home} from './android/app/src/assets/Home'
-
 
 function App() {
   return (
-     <Provider store={store}>
       <Calculator/>
-     </Provider>
   )
 }
 
